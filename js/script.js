b9306ca8d32f87503c7c3038d39d47aebed8e32f
marcoche88 <marco.che@hotmail.it>
2021-07-28 16:37:25 +0200

    dots slider

--- js/script.js
+++ js/script.js
@@ -12,14 +12,20 @@ const app = new Vue({
         ],
     },
     methods: {
+        // verifica se l'indice dell'array corrisponde con quello del numero attivo
         isActive(index) {
             return (index === this.activeNumber) ? 'active' : '';
         },
+        // fa scorrere lo slider a destra fino alla lunghezza massima dell'array per poi riiniziare da 0
         slideRight() {
             this.activeNumber = (this.activeNumber === this.images.length - 1) ? 0 : this.activeNumber + 1;
         },
+        // fa scorrere lo slider a sinistra fino allo 0 per poi riiniziare dalla lunghezza massima dell'array 
         slideLeft() {
             this.activeNumber = (this.activeNumber === 0) ? this.images.length - 1 : this.activeNumber - 1;
+        },
+        changeImage(index) {
+            this.activeNumber = index;
         },
     },
 });
