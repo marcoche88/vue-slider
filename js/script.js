785ff7b9a63ca650bea1d864ded67dca299771de
marcoche88 <marco.che@hotmail.it>
2021-07-28 15:59:42 +0200

    slider img

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 Vue.config.devtools = true;
 
+const app = new Vue({
+    el: "#root",
+    data: {
+        activeNumber: 0,
+        images: [
+            "./img/image1.jpg",
+            "./img/image2.jpg",
+            "./img/image3.jpg",
+            "./img/image4.jpg"
+        ],
+    },
+    methods: {
+        isActive(index) {
+            return (index === this.activeNumber) ? 'active' : ''
+        },
+    },
+});
